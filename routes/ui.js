b64c155f76ec417afc9fa7c7115d4048e3d5ec8a
FAREAST\xumia <xumia@microsoft.com>
2021-05-24 07:34:30 +0800

    Fix bug

--- routes/ui.js
+++ routes/ui.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const request = require('sync-request');
 const constants = require('./constants');
 const kusto = require('./kusto');
+const util = require('util');
+
+const buildResultUrlFormat = "https://dev.azure.com/mssonic/build/_build/results?buildId=%s&view=artifacts&pathAsName=false&type=publishedArtifacts";
 
 const query_sonicimagebuilds = 'GetSonicImageBuilds()  | order by Platform asc, SourceBranch desc | project Sequence=row_number(), DefinitionId, DefinitionName, Platform, SourceBranch'
 
@@ -30,9 +34,29 @@ router.get('/sonic/builds/:buildid/artifacts', function(req, res, next) {
 
 /* Get SONiC artifact files. */
 router.get('/sonic/builds/:buildid/artifacts/:artifactId', function(req, res, next) {
-    console.log(req);
-    console.log(constants.NAVIGATOR_BARTIFACTS);
-    res.render('builds', { title: 'Builds', navigators:constants.NAVIGATOR_ARTIFACTS });
+    var params = req.params;
+    var query = req.query;
+    var sourceUrl = util.format(buildResultUrlFormat, params.buildId);
+    var url = 'https://dev.azure.com/mssonic/_apis/Contribution/HierarchyQuery/project/be1b070f-be15-4154-aade-b1d3bfb17054';
+    var body = {"contributionIds":["ms.vss-build-web.run-artifacts-data-provider"],
+    "dataProviderContext":{"properties":{
+      "artifactId":params.artifactId,
+      "buildId":params.buildId,
+      "sourcePage":{
+        "url": sourceUrl,
+        "routeValues":{"project":"build","action":"Execute"}
+      }}}};
+    var options = {
+      headers: {'accept': 'application/json;api-version=5.0-preview.1',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
+    var artifactsRes = request('POST', url, options);
+    var artifacts = JSON.parse(artifactsRes.getBody('utf8'));
+    var dataProvider = artifacts['dataProviders']['ms.vss-build-web.run-artifacts-data-provider'];
+    res.write(JSON.stringify(dataProvider));
+    res.end();
   });
 
 module.exports = router;
